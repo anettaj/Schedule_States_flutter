01f54a940388ae4c9553b7900db2c3399585771a
Anet Taj <58222128+anettaj@users.noreply.github.com>
2024-03-24 01:48:01 +0530

    constance added

---
+++ lib/Components/constance.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/material.dart';
+
+//togglebutton
+Color kinactiveThumbColor=Colors.deepPurple.shade800;
+const kactiveColor=Colors.white;
+const kactiveTrackColor=Colors.green;
+const Color kinactiveTrackColor=Colors.black12;
+const kinactiveOutlineColor=Colors.green;
+const kactiveOutlineColor=Colors.transparent;
+
+//scheduleCard
+const kButtonStyleDataColor=Color(0xffF6F3FA);
+const kDropDownTextStyle=TextStyle(
+  letterSpacing: 0.015,
+  fontSize: 16,
+);
+const kWeekNameStyle=TextStyle(
+  fontSize: 16,
+  color: Colors.black,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0.1,
+  wordSpacing: 1,
+);
+const kThemeDividerColor=Colors.transparent;

5a0c01d8bf49eb02f66c7a8d809abff9bbd5383b
Anet Taj <58222128+anettaj@users.noreply.github.com>
2024-03-27 23:25:27 +0530

    changed colour to (0xff4B0D55)

--- lib/Components/constance.dart
+++ lib/Components/constance.dart
@@ -1,16 +1,15 @@
-
 import 'package:flutter/material.dart';
 
 //togglebutton
-Color kinactiveThumbColor=Colors.deepPurple.shade800;
+Color kinactiveThumbColor=Color(0xff4B0D55);
 const kactiveColor=Colors.white;
-const kactiveTrackColor=Colors.green;
+const kactiveTrackColor=Color(0xff6CDB98);
 const Color kinactiveTrackColor=Colors.black12;
 const kinactiveOutlineColor=Colors.green;
 const kactiveOutlineColor=Colors.transparent;
 
 //scheduleCard
-const kButtonStyleDataColor=Color(0xffF6F3FA);
+const kButtonStyleDataColor=Color(0xffF6F4FA);
 const kDropDownTextStyle=TextStyle(
   letterSpacing: 0.015,
   fontSize: 16,
@@ -23,3 +22,5 @@ const kWeekNameStyle=TextStyle(
   wordSpacing: 1,
 );
 const kThemeDividerColor=Colors.transparent;
+
+Color  HomeThemeColor=Color(0xff4B0D55);
